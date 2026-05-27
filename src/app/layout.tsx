7629f095aa1a4119f8ed9c308fd3b2eca920649e
Keith <kayceecode@gmail.com>
2026-05-27 17:44:58 -0400

    favicon edit & moved to src/

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,14 +14,6 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
 
-        <link rel="icon" type="image/png" href="/favicon-96x96.png?v=20260331" sizes="96x96" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg?v=20260331" />
-        <link rel="shortcut icon" href="/favicon.ico?v=20260331" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=20260331" />
-        <meta name="apple-mobile-web-app-title" content="MyWebSite" />
-        <link rel="manifest" href="/site.webmanifest?v=20260331" />
-
-
         {/* <link rel="icon" type="image/png" href="/favicon.ico/favicon-96x96.png?v=20260329" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/favicon.ico/favicon.svg?v=20260329" />
         <link rel="shortcut icon" href="/favicon.ico/favicon.ico?v=20260329" />
